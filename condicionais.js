ece8cf4ac7476f60ca140d9f529314d059624498
FIC <fic@info.local>
2025-04-03 21:53:43 -0300

    Trabalho Java

---
+++ condicionais.js
@@ -0,0 +1,47 @@
+// let numero = prompt("Digite um numero:")
+
+// if (numero > 0) {
+    
+//     console.log("Verdade")
+
+// } else {
+
+//     if (numero < 0){
+
+//         console.log("Mentira")
+
+// } else {
+
+//         console.log("é zero")
+
+//     }
+// }
+
+// let numero = prompt("Qual sua idade?:")
+
+// if (numero >= 18) {
+
+//     console.log("maior de idade")
+
+// } else {
+
+//     console.log("menor de idade")
+    
+// }
+
+// let numero = prompt("Digite um numero divisivel por 2:")
+
+// if (numero %2==0) {
+
+//     console.log("certo")
+
+// } else {
+
+//     console.log("errado")
+
+// }
+
+let numero = prompt("Qual sua idade?:")
+if (numero >= 0) {
+    console("Criança")
+}
