264fc6a01cc59d1670cf4b72f9259f18e6f83884
FIC <fic@info.local>
2025-04-04 21:51:17 -0300

    Indo para Intermidiario

--- condicionais.js
+++ condicionais.js
@@ -1,47 +1,130 @@
-// let numero = prompt("Digite um numero:")
 
-// if (numero > 0) {
-    
-//     console.log("Verdade")
+function basico01() {
 
-// } else {
+    // debugger;
 
-//     if (numero < 0){
+    let numero = prompt("Qual sua idade?:")
 
-//         console.log("Mentira")
+    if (numero >= 18) {
 
-// } else {
+        alert("maior de idade")
 
-//         console.log("é zero")
+    } else {
 
-//     }
-// }
+        alert("menor de idade")
 
-// let numero = prompt("Qual sua idade?:")
+    }
+}
 
-// if (numero >= 18) {
+function basico02() {
 
-//     console.log("maior de idade")
+    let numero = prompt("Insira um número de sua escolha:")
 
-// } else {
+    if (numero >= 0) {
 
-//     console.log("menor de idade")
-    
-// }
+        alert("Número Positivo!")
 
-// let numero = prompt("Digite um numero divisivel por 2:")
+    } else {
 
-// if (numero %2==0) {
+        alert("Número Negativo!")
+    }
+}
 
-//     console.log("certo")
+function basico03() {
 
-// } else {
+    let nota = prompt("Qual a nota do Aluno?:")
 
-//     console.log("errado")
+    if (nota >= 60) {
 
-// }
+        alert("Passou!")
 
-let numero = prompt("Qual sua idade?:")
-if (numero >= 0) {
-    console("Criança")
+    } else {
+
+        alert("Vou nem fala!")
+
+    }
+}
+
+function basico04() {
+
+    let numero = prompt("Digite um numero:")
+
+    if (numero > 0) {
+
+        alert("Verdade")
+
+    } else {
+
+        if (numero < 0) {
+
+            alert("Mentira")
+
+        } else {
+
+            alert("é zero")
+
+        }
+    }
+}
+
+function basico05() {
+
+    let idade = prompt("Qual sua idade:")
+
+    if (idade >= 18) {
+
+        alert("Adulto")
+
+    } else { }
+
+    if (idade >= 13 && idade <= 17) {
+
+        alert("Adolescente")
+
+    } else {
+
+        if (idade >= 0 && idade <= 12) {
+
+            alert("Criança")
+        }
+
+    }
+}
+
+function basico06() {
+
+    let numero = prompt("Digite um numero divisivel por 2:")
+
+    if (numero % 2 == 0) {
+
+        alert("certo")
+
+    } else {
+
+        alert("errado")
+
+    }
+}
+
+function intermediario01() {
+
+    let num1 = Number(prompt("Escolha um numero:"))
+    let operacao = prompt("Qual operacao?:")
+    let num2 = Number(prompt("Escolha mais um numero:"))
+
+
+    if (operacao === "+") {
+        alert(num1 + num2)
+    } else if (operacao === "-") {
+        alert(num1 - num2)
+    } else if (operacao === "*") {
+        alert(num1 * num2)
+    } else if (operacao === "/") {
+        alert(num1 / num2)
+    }
+}
+
+function intermediario03() {
+
+    let valor = Number(prompt("Qual o Valor da compra?:"))
 }
